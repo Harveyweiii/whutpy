cd70ae28db66b651844b13141352e03628dbdd71
weijunhui <876888093@qq.com>
2022-09-28 16:49:50 +0800

    models.py

--- whutpy/models.py
+++ whutpy/models.py
@@ -12,12 +12,20 @@ class WhutUrl:
 
 
 class Endpoints:
+    #CWSF
     CWSF_LOGIN=WhutUrl.CWSF_URL+'innerUserLogin'
     QUERY_RESERVE=WhutUrl.CWSF_URL+'queryReserve'
     PAY_LIST=WhutUrl.CWSF_URL+'queryPayOrderList'
+
+    #WHUT_NET
     NET_AUTH=WhutUrl.WHUT_NET_AUTH+'include/auth_action.php'
+
+    #ZHLGD
     ZHLGD_LOGIN=WhutUrl.ZHLGD_URL+'tpass/login'
     CARD_CONSUM_API=WhutUrl.ZHLGD_URL+'tp_up/up/sysintegration/getCardConsumList'
+    TP_UP=WhutUrl.ZHLGD_URL+'tp_up/'
+    GET_CARD_MONEY=WhutUrl.ZHLGD_URL+'tp_up/up/sysintegration/getCardMoney'
+    UNREADE_MESSAGE=WhutUrl.ZHLGD_URL+'tp_up/sys/uacm/msgcenter/getSelUnReadCount'
 
 
 class Headers:
@@ -47,8 +55,49 @@ class Headers:
         'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
-        # 'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/x-www-form-urlencoded',
         'Host': 'zhlgd.whut.edu.cn',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
     }
+    GET_CARD_MONEY_HEADERS={
+        'Accept':'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding':'gzip, deflate',
+        'Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection':'keep-alive',
+        'Content-Length':'2',
+        'Content-Type':'application/json;charset=UTF-8',
+        'Host':'zhlgd.whut.edu.cn',
+        'Origin':'http://zhlgd.whut.edu.cn',
+        'Referer':'http://zhlgd.whut.edu.cn/tp_up/view?m=up',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+        'X-Requested-With':'XMLHttpRequest',
+    }
+
+    GET_CARD_CONSUMLIST_HEADERS={
+        'Accept':'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding':'gzip, deflate',
+        'Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection':'keep-alive',
+        'Content-Length':'130',
+        'Content-Type':'application/json;charset=UTF-8',
+        'Cookie':'Language=zh_CN; JSESSIONID=fABlNz-zNzsrvAD0a9OiG1MyN_NRU7B-ntV_i2HOiFFctL66ZVOV!-1701493779; tp_up=h7plOsOBbWWK5BDcVjXFtC95FN79N8GioHZcCXxYbl4ojYxuJJGF!59120401',
+        'Host':'zhlgd.whut.edu.cn',
+        'Origin':'http://zhlgd.whut.edu.cn',
+        'Referer':'http://zhlgd.whut.edu.cn/tp_up/view?m=up',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+        'X-Requested-With':'XMLHttpRequest',
+    }
+    UNREAD_MESSAGE_HEADERS={
+            'Accept':'application/json, text/javascript, */*; q=0.01',
+            'Accept-Encoding':'gzip, deflate',
+            'Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Connection':'keep-alive',
+            'Content-Length':'2',
+            'Content-Type':'application/json;charset=UTF-8',
+            'Host':'zhlgd.whut.edu.cn',
+            'Origin':'http://zhlgd.whut.edu.cn',
+            'Referer':'http://zhlgd.whut.edu.cn/tp_up/view?m=up',
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+            'X-Requested-With':'XMLHttpRequest',
+        }
